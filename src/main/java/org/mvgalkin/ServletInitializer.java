27497b66a623ab60155ae6fc9f61acf6f9980822
Галкин Максим <m.galkin@bftcom.com>
2022-01-21 18:10:01 +0400

    Прикрутил Liquibase

--- src/main/java/org/mvgalkin/ServletInitializer.java
+++ src/main/java/org/mvgalkin/ServletInitializer.java
@@ -1,7 +1,9 @@
 package org.mvgalkin;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
