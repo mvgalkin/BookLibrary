be9ee1ae242e384086d093565228f3f8a65a7999
Галкин Максим <m.galkin@bftcom.com>
2022-01-23 23:45:41 +0400

    Реализация сервиса - но почему-то DAO не находит

--- src/main/java/org/mvgalkin/dao/BooksInfoDaoRepository.java
+++ src/main/java/org/mvgalkin/dao/BooksInfoDaoRepository.java
@@ -3,10 +3,9 @@ package org.mvgalkin.dao;
 import org.mvgalkin.models.BookInfo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface BooksInfoDaoRepository extends CrudRepository<BookInfo, Long> {
+public interface BooksInfoDaoRepository extends PagingAndSortingRepository<BookInfo, Long> {
     Page<BookInfo> findByNameIsLike(String partOfName, Pageable pageable);
     Page<BookInfo> findByAuthors_NameLike(String authors_name, Pageable pageable);
     Page<BookInfo> findByGenres_NameLike(String genres_name, Pageable pageable);
